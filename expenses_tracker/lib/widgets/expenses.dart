f5e77defb3110b39ac259260aa08771f0f7e457a
DESKTOP-MOUI7DH\Pongo <ijlalnurhadi01@gmail.com>
2024-08-27 13:05:34 +0800

    Expenses Tracker - Perbaiki modal dan tambahkan snakebar

--- expenses_tracker/lib/widgets/expenses.dart
+++ expenses_tracker/lib/widgets/expenses.dart
@@ -40,6 +40,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+        isScrollControlled: true,
         context: context,
         builder: (ctx) => NewExpense(onAddExpense: _addExpense));
   }
@@ -50,8 +51,40 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('Expense removed'),
+        duration: const Duration(seconds: 3),
+        action: SnackBarAction(
+          label: 'UNDO',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("No expenses found. Start adding some!"),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expenses Tracker'),
@@ -65,10 +98,7 @@ class _ExpensesState extends State<Expenses> {
         ],
       ),
       body: Column(
-        children: [
-          const Text('The chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses))
-        ],
+        children: [const Text('The chart'), Expanded(child: mainContent)],
       ),
     );
   }
